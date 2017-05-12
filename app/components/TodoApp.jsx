a648b66659e5a9114e2077367fb021dfc9dbada2
Jatto Ade <jattoade@gmail.com>
2017-05-12 01:00:10 +0100

    Added TodoApp Wrapper Component and test file

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+    render: function () {
+        return(
+          <div>
+              React Todo App
+          </div>
+        );
+    }
+});
+
+module.exports = TodoApp;
